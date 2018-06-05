b977c4ad1634f411fd663735197224191809961a
Zach Harris <zachbharris94@gmail.com>
2018-06-05 13:26:54 -0500

    graphql fun and indexing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,33 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <div>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
+    <h2>Index</h2>
+    {data.allMarkdownRemark.edges.map(post => (
+      <a key={post.node.id} href={post.node.frontmatter.path}>{post.node.frontmatter.title}</a>
+    ))}
   </div>
 )
+
+export const pageQuery = graphql`
+  query indexQuery {
+    allMarkdownRemark(limit: 6) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            path
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
