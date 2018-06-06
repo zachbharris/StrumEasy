ff2461226dc48ecb7dddeb572d2d1a21b90d3431
Zach Harris <zachbharris94@gmail.com>
2018-06-06 09:27:00 -0500

    favicon and asset dir

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Header from '../components/header'
-import '../scss/index.scss'
+import '../assets/scss/index.scss'
+import Favicon from '../assets/images/favicon.png'
 
 const Layout = ({ children, data }) => (
   <div>
@@ -13,19 +14,13 @@ const Layout = ({ children, data }) => (
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
       ]}
+      link={[
+        { rel: 'icon', href: `${Favicon}` }
+      ]}
     />
     
     <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+    {children()}
   </div>
 )
 
